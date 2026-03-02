1dcbff205af0e79eadcd31b7d6e99e06a756fa3a
Khalid-Siddiqi <khalid20031016@gmail.com>
2026-03-02 12:29:01 +0500

    new libraries added

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ import streamlit as st
 
 df = pd.read_csv(
     io = "supermarkt_sales.xlsx",
-    engine= "openpyxl")
+    engine= "openpyxl",
+    sheet_name= "Sales",
+    skiprows=3,
+    usecols="B:R",
+    nrows=1000,
+    )
+
+print(df.head())
